ec0d459da3aa68f9434567e42590e79a67daf361
Oscar Carvente <oscar.carvente@gmail.com>
2024-04-11 12:53:25 -0700

    Added new fact

--- facts.sh
+++ facts.sh
@@ -7,6 +7,7 @@ facts=(
 	"The state tree of Washington is the Western Hemlock."
 	"All mammals get goosebumps."
 	"Queen Alexandra's Birdwing is the largest butterfly in the world: females can reach wingspans of 28cm (11 inches)."
+"Stanford football was the best in the PAC12"
 )
 
 # array to keep track of which facts have been viewed
